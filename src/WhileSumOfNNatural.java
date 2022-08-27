8f7eee935b4869d4cc5917b4e184c24cfa9f0f44
srushti-Mane <srushtideshmukh2909@gmail.com>
2022-08-27 18:30:13 +0530

    modified:code for sum of n natural number

---
+++ src/WhileSumOfNNatural.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class WhileSumOfNNatural {
+    public static void main(String[] args){
+            int x, i=1;
+            int sum=0;
+            System.out.println("enter the number : ");
+            Scanner sc=new Scanner(System.in);
+            x=sc.nextInt();
+            while(i <= x)
+            {
+                sum = sum + x;
+                i++;
+            }
+            System.out.println("sum of "+x+" number is : " +sum);
+        }
+    }
+
